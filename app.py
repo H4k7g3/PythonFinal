9d1a69bec74f829bb79fcc0cc362bd6086e37034
H4k7g3 <94833372+H4k7g3@users.noreply.github.com>
2022-11-08 04:20:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import UserMixin,login_user,LoginManager,current_user,logout_user,login_required
 
-
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
 
 
 def create_app():
